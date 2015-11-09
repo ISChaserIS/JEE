40479093a3548a90db521d4140a0e043294d97f4
Demonlord32 <suspectolcha@gmail.com>
2015-11-09 18:58:53 +0600

    Initial commit

---
+++ src/TryLogin.java
@@ -0,0 +1,26 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Olcha on 06.11.2015.
+ */
+@WebServlet("/login")
+public class TryLogin extends HttpServlet{
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException{
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        resp.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+
+        if (username.equals("master") && password.equals("yoda")){
+            out.println("Welcome");
+        }else {
+            out.println("Incorrect login or password");
+        }
+    }
+}
---
+++ web/js/script.js
@@ -0,0 +1,123 @@
+var currentIndex = 0;
+
+function hello(){
+	var ajax = new XMLHttpRequest();
+	var xxx = document.getElementById("hey1").value;
+	var zzz = document.getElementById("hey2").value;
+	ajax.open('GET', 'http://127.0.0.1:8080/JEETest/login?username=' + xxx + '&password=' + zzz, false);
+	var yyy =  ajax.responseText;
+	alert(yyy);
+}
+
+function getDataFromServer () {
+	var ajax = new XMLHttpRequest();
+	ajax.open('GET', 'http://smart-route.ru:8100/adapter-web/rest/dictionary/c580d006-f86f-4bdd-84be-b51de6f626c6', false);
+	ajax.send();
+	var response = ajax.responseText;
+	return response;
+}
+
+function parseData(){
+	var json = getDataFromServer();
+	var data = JSON.parse(json);
+	var arr = [];
+	arr = data.documents;
+	return arr;
+}
+function setStrings(){
+	var data = parseData();
+	data.sort(function(obj1, obj2)
+	{
+		if (obj1.fio < obj2.fio) return -1;
+  		if (obj1.fio > obj2.fio) return 1;
+  			return 0;
+  	});
+	
+	for (var i = 0; i < 10; i++) {
+		var a = document.createElement('p');
+		a.innerHTML = i + " " + data[i].fio + " " + data[i].gender + " " + data[i].birthDate + " " + data[i].phone;
+		var b = document.getElementById("content3");
+		b.appendChild(a);
+	};
+}
+
+function searchValue(){
+	
+	var data = parseData();
+	data.sort(function(obj1, obj2)
+	{
+		if (obj1.fio < obj2.fio) return -1;
+  		if (obj1.fio > obj2.fio) return 1;
+  			return 0;
+  	});
+  	var jq = jQuery('p');
+	jq.remove();
+	var str = document.getElementById('textEdit1').value;
+	str = str.toLowerCase();
+	if(str.length == 0)
+	{
+	  	setStrings();
+	  	return;
+	}
+	var array = [];
+	//var rexp = new RegExp(str);
+	for (var i = 0, j = 0; i < data.length; i++) {
+		var buff = data[i].fio.toLowerCase();
+		if (buff.indexOf(str) != -1) {
+		   	array[j] = data[i];
+		   	j++;
+  		};
+	}
+	data = array;
+	for (var i = 0; i < data.length; i++) {
+		var a = document.createElement('p');
+		a.innerHTML =data[i].fio + " " + data[i].gender + " " + data[i].birthDate + " " + data[i].phone;
+		var b = document.getElementById("content3");
+		b.appendChild(a);
+	}
+}
+
+function previousPage(){
+	
+	var data = parseData();
+	data.sort(function(obj1, obj2)
+	{
+		if (obj1.fio < obj2.fio) return -1;
+  		if (obj1.fio > obj2.fio) return 1;
+  			return 0;
+  	});
+	if (currentIndex < 10) {
+		console.log("STOP");
+	}else{
+		currentIndex -= 10;
+		console.log("Index: " + currentIndex);
+		var jq = jQuery('p');
+		jq.remove();
+		for (var i = currentIndex; i < currentIndex + 10; i++) {
+			var a = document.createElement('p');
+			a.innerHTML = i + " " + data[i].fio + " " + data[i].gender + " " + data[i].birthDate + " " + data[i].phone;
+			var b = document.getElementById("content3");
+			b.appendChild(a);
+		};
+	}
+}
+
+function nextPage(){
+	currentIndex += 10;
+	var jq = jQuery('p');
+	jq.remove();
+	var data = parseData();
+	data.sort(function(obj1, obj2)
+	{
+		if (obj1.fio < obj2.fio) return -1;
+  		if (obj1.fio > obj2.fio) return 1;
+  			return 0;
+  	});
+  	for (var i = currentIndex; i < currentIndex + 10; i++) {
+		var a = document.createElement('p');
+		a.innerHTML = i + " " + data[i].fio + " " + data[i].gender + " " + data[i].birthDate + " " + data[i].phone;
+		var b = document.getElementById("content3");
+		b.appendChild(a);
+	};
+	console.log("Index: " + currentIndex);
+}
