eff7159209c0fd34294bf9c8c5336c27ba167eb7
Demonlord32 <suspectolcha@gmail.com>
2015-11-12 23:41:54 +0600

    The bug in script.js

--- src/TryLogin.java
+++ src/TryLogin.java
@@ -1,10 +1,15 @@
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.io.StringReader;
 
 /**
  * Created by Olcha on 06.11.2015.
@@ -12,8 +17,16 @@ import java.io.PrintWriter;
 @WebServlet("/login")
 public class TryLogin extends HttpServlet{
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException{
-        String username = req.getParameter("username");
-        String password = req.getParameter("password");
+
+        JsonReader reader = Json.createReader(req.getInputStream());
+        JsonObject newJson = reader.readObject();
+        reader.close();
+
+        newJson = newJson.getJsonObject("json");
+
+        String username = newJson.getString("login");
+        String password = newJson.getString("password");
+
         resp.setContentType("text/html; charset=UTF-8");
         PrintWriter out = resp.getWriter();
 
